74cba7219275b3f91cc35709545fef116025dc66
nicdims0111-well <nicdims0111@gmail.com>
2025-12-22 13:36:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+import plotly as px
 
-st.write("Hello, *MOKKK!* :sunglasses:")
-
-st.write("WKWKWKWKWKKWK")
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
